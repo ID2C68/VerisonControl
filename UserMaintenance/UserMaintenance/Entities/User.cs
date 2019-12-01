ca2854cf1e993ff5593a78a8b95cb15e9171f37b
Dávid Koleszár <david.koleszar@stud.uni-corvinus.hu>
2019-12-01 13:43:40 +0100

    Save file

--- UserMaintenance/UserMaintenance/Entities/User.cs
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -9,6 +9,6 @@ namespace UserMaintenance.Entities
     public class User
     {
         public Guid ID { get; set; } = Guid.NewGuid();
-        public string FullName { get; set; };
+        public string FullName { get; set; }
     }
 }
--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,7 @@ namespace UserMaintenance
             InitializeComponent();
             label1.Text=Resource1.FullName;
             button1.Text = Resource1.Add;
-
+            button2.Text = Resource1.SaveFile;
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
             listBox1.DisplayMember = "FullName";
@@ -29,9 +30,26 @@ namespace UserMaintenance
         {
             var u = new User()
             {
-                FullName = label1.Text,
+                FullName = textBox1.Text,
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog()==DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(sfd.FileName);
+                foreach (var s in users)
+                {
+                    sw.Write(s.ID);
+                    sw.Write(";");
+                    sw.Write(s.FullName);
+                    sw.WriteLine();
+                }
+                sw.Close();
+            }
         }
     }
 }
